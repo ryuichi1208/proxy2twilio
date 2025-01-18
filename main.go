4d33856de16924b70adf9cc77d8dc67829bbf254
ryuichi1208 <ryucrosskey@gmail.com>
2025-01-18 21:20:28 +0900

    add: version print options

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 )
 
+var version = "v0.0.1"
+
 func run() error {
 	if err := startHTTPServer(); err != nil {
 		return fmt.Errorf("failed to start HTTP server: %w", err)
@@ -14,6 +16,12 @@ func run() error {
 }
 
 func main() {
+	// versionという引数が指定された場合はバージョン情報を表示する
+	if len(os.Args) == 2 && os.Args[1] == "version" {
+		fmt.Fprintf(os.Stdout, "version: %s\n", version)
+		os.Exit(0)
+	}
+
 	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
